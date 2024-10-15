64dd21d8291b3207908bb885bb906839b79ca574
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-10-15 16:35:49 +0900

    問題文が曖昧で実装が出来ない

---
+++ intermediate/reach_fund.go
@@ -0,0 +1,21 @@
+package intermediate
+
+func HowLongToReachFundGoal(capitalMoney int32, goalMoney int32, interest int32) int32 {
+	if capitalMoney >= goalMoney {
+		return 0
+	}
+	currentCapitalMoney := float64(capitalMoney)
+	currentgoalMoney := float64(goalMoney)
+	for currentYear := 0; currentYear <= 80; currentYear++ {
+		currentCapitalMoney *= float64(float64(interest)/100 + 1)
+		if currentYear%2 == 0 {
+			currentgoalMoney *= 1.03
+		} else {
+			currentgoalMoney *= 1.02
+		}
+		if currentCapitalMoney >= currentgoalMoney {
+			return int32(currentYear)
+		}
+	}
+	return 80
+}
