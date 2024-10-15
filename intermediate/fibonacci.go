df2bc20103da90b1abf83a550ba14efd89cf4efd
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-10-15 16:37:12 +0900

    末尾再帰の実装

---
+++ intermediate/fibonacci.go
@@ -0,0 +1,22 @@
+package intermediate
+
+func FibonacciTail(n int32) int32 { // 末尾再帰を実装
+	return fibonacciTailHelper(0, 1, n)
+}
+
+func fibonacciTailHelper(f1, f2, n int32) int32 {
+	if n == 0 {
+		return f1
+	}
+	return fibonacciTailHelper(f2, f1+f2, n-1)
+}
+
+// func Fibonacci(n int32) int32 { //再帰関数実装
+// 	if n == 0 {
+// 		return 0
+// 	}
+// 	if n == 1 {
+// 		return 1
+// 	}
+// 	return Fibonacci(n-1) + Fibonacci(n-2)
+// }
