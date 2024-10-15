7cc05acc1c296aa3bc18286746749240b14ed55e
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-10-15 16:35:31 +0900

    solved

---
+++ intermediate/divisor.go
@@ -0,0 +1,15 @@
+package intermediate
+
+import "strconv"
+
+func Divisor(number int32) string {
+	var divisors string
+	for i := int(number); i > 0; i-- {
+		if i == 1 {
+			divisors += strconv.Itoa(int(number) / i)
+		} else if int(number)%i == 0 {
+			divisors += strconv.Itoa(int(number)/i) + "-"
+		}
+	}
+	return divisors
+}
